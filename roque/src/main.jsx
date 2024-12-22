f9053170e9ad5dbb77c3ba35c8487ed4c75ce889
Jair <jairestebanca@gmail.com>
2024-12-21 22:27:48 -0500

    Elimina archivos que aun no se usarán y actualiza main para usar App.jsx

--- roque/src/main.jsx
+++ roque/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import { StrictMode, Fragment } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Fragment>
+      <App />
+    </Fragment>
+  </StrictMode>
 )
